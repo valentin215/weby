2830e6c093cc1e019940be10eafb33d285e09a3d
Valentin Birault <valentin@hivebrite.com>
2024-12-08 12:00:45 +0100

    fix: remove raise example

--- controllers/articles_controller.rb
+++ controllers/articles_controller.rb
@@ -3,8 +3,6 @@
 require_relative 'application_controller'
 class ArticlesController < ApplicationController
   def index
-    raise 'Stack Overflow!'
-
     @title = 'Rebuild Rails Framework !'
     @tagline = 'A simple repository to understand the foundations of Rails'
   end
