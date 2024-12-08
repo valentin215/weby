b2116e422d0104ed367c16f1f0666a185662de7f
Valentin Birault <valentin@hivebrite.com>
2024-12-08 11:59:05 +0100

    chore: adding logging system

--- app.rb
+++ app.rb
@@ -1,17 +1,30 @@
+require 'logger'
 require_relative './config/routes'
 
+# Rack application
 class App
+  attr_reader :logger
+
+  def initialize
+    @logger = Logger.new('log/development.log')
+  end
 
   def call(env)
-    headers = {"Content-Type" => "text/html"}
+    logger.info("#{env['REQUEST_METHOD']}: #{env['REQUEST_PATH']}")
+
+    headers = { 'Content-Type' => 'text/html' }
 
     response_html = router.build_response(env)
 
     [200, headers, [response_html]]
+  rescue StandardError => e
+    logger.add(Logger::ERROR, e)
+
+    # we still have to return a rack-compliant response
+    [200, headers, ["Error: #{e.message}"]]
   end
 
   private
 
   def router = Router.instance
-
 end
--- controllers/articles_controller.rb
+++ controllers/articles_controller.rb
@@ -3,6 +3,8 @@
 require_relative 'application_controller'
 class ArticlesController < ApplicationController
   def index
+    raise 'Stack Overflow!'
+
     @title = 'Rebuild Rails Framework !'
     @tagline = 'A simple repository to understand the foundations of Rails'
   end
