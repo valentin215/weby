f19a612f856700a8a918131771cfee1127db103f
Valentin Birault <valentin@hivebrite.com>
2024-12-24 16:05:34 +0100

    fix: removed ordering

--- bin/active_record.rb
+++ bin/active_record.rb
@@ -25,16 +25,19 @@ class User < ActiveRecord::Base
   def self.active
     where(active: 1)
   end
-
-  def self.recent
-    order(created_at: :asc)
-  end
 end
 
 # Query Example
-puts User.where(organization: 'RubyCademy').active.recent.count
-puts User.active.where(organization: 'RubyCademy').recent.to_a
+puts User.where(organization: 'RubyCademy').active.count
+puts User.active.where(organization: 'RubyCademy').to_a
 
 # Execution
 # chmod +x bin/active_record.rb
 # .bin/active_record.rb
+
+# Output
+# Executing SQL: SELECT COUNT(*) FROM users WHERE organization = 'RubyCademy' AND active = '1'
+# 2
+# Executing SQL: SELECT * FROM users WHERE active = '1' AND organization = 'RubyCademy'
+# {"id"=>1, "name"=>"Alice", "organization"=>"RubyCademy", "active"=>1, "created_at"=>"2023-03-01"}
+# {"id"=>3, "name"=>"Charlie", "organization"=>"RubyCademy", "active"=>1, "created_at"=>"2022-02-01"}
--- lib/active_record/base.rb
+++ lib/active_record/base.rb
@@ -1,61 +1,64 @@
 module ActiveRecord
   class Base
-    class << self
-      attr_reader :relation_delegate_cache
+    def self.inherited(subclass)
+      subclass.initialize_relation_delegate_cache
+    end
 
-      def inherited(subclass)
-        subclass.initialize_relation_delegate_cache
-      end
+    def self.initialize_relation_delegate_cache
+      @relation_delegate_cache = {}
+      Delegation::DELEGATED_CLASSES.each do |klass|
+        delegate =
+          Class.new(klass) do
+            include ClassSpecificRelation
+          end
 
-      def self.initialize_relation_delegate_cache
-        @relation_delegate_cache = {}
-        Delegation::DELEGATED_CLASSES.each do |klass|
-          delegate =
-            Class.new(klass) do
-              include ClassSpecificRelation
-            end
+        mangled_name = klass.name.gsub('::', '_')
+        const_set mangled_name, delegate
+        private_constant mangled_name
 
-          mangled_name = klass.name.gsub('::', '_')
-          const_set(mangled_name, delegate)
-          private_constant(mangled_name)
+        @relation_delegate_cache[klass] = delegate
 
-          @relation_delegate_cache[klass] = delegate
-
-          current_scope.scope = relation_delegate_cache[Relation].new(self)
-        end
-      end
-
-      def where(conditions) = current_scope.scope.where(conditions)
-
-      def order(conditions) = current_scope.scope.order(conditions)
-
-      def count
-        sql = current_scope.scope.to_sql
-        query = "SELECT COUNT(*) FROM #{table_name} #{sql}"
-        flush_and_execute(query)
-      end
-
-      def to_a
-        sql = current_scope.scope.to_sql
-        query = "SELECT * FROM #{table_name} #{sql}"
-        flush_and_execute(query)
-      end
-
-      def table_name = "#{name.downcase}s"
-
-      def current_scope
-        @current_scope ||= CurrentScope.new
-      end
-
-      def flush_current_scope
         current_scope.scope = relation_delegate_cache[Relation].new(self)
       end
+    end
 
-      def flush_and_execute(query)
-        puts "Executing SQL: #{query}"
-        flush_current_scope
-        DatabaseConnection.instance.execute(query)
-      end
+    class << self
+      attr_reader :relation_delegate_cache
+    end
+
+    def self.where(conditions) = current_scope.scope.where(conditions)
+    def self.order(ordering) = current_scope.scope.order(ordering)
+
+    def self.count
+      sql = current_scope.scope.to_sql
+      query = "SELECT COUNT(*) FROM #{table_name} #{sql}"
+
+      puts "Executing SQL: #{query}"
+
+      flush_current_scope
+      DatabaseConnection.instance.execute(query).first['COUNT(*)']
+    end
+
+    def self.to_a
+      sql = current_scope.scope.to_sql
+      query = "SELECT * FROM #{table_name} #{sql}"
+
+      puts "Executing SQL: #{query}"
+
+      flush_current_scope
+      DatabaseConnection.instance.execute(query)
+    end
+
+    def self.table_name
+      name.downcase + 's'
+    end
+
+    def self.current_scope
+      @current_scope ||= CurrentScope.new
+    end
+
+    def self.flush_current_scope
+      current_scope.scope = relation_delegate_cache[Relation].new(self)
     end
   end
 end
