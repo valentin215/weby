61fd9c1516e0bde34273610c73a8f3c646ecf8d9
Valentin Birault <valentin@hivebrite.com>
2024-12-31 11:52:57 +0100

    chore: adding ordering

--- lib/active_record/relation.rb
+++ lib/active_record/relation.rb
@@ -4,11 +4,16 @@ class Relation
   def initialize(model)
     @model = model
     @where_values = []
+    @order_values = []
   end
 
   # @param conditions [Hash]
   # e.g. { name: 'Alice', age: 20 }
   def where(conditions) = tap { @where_values << conditions }
+
+  # @param ordering [String]
+  # e.g. 'name ASC'
+  def order(ordering) = tap { @order_values << ordering }
 
   def to_sql
     where_clause = if @where_values.empty?
@@ -21,6 +26,16 @@ class Relation
                      end.join(' AND ')}"
                    end
 
-    [where_clause].reject(&:empty?).join(' ')
+    order_clause = if @order_values.empty?
+                     ''
+                   else
+                     "ORDER BY #{@order_values.map do |cond|
+                       cond.map do |k, v|
+                         "#{k} #{v.upcase}"
+                       end.join(', ')
+                     end.join(', ')}"
+                   end
+
+    [where_clause, order_clause].reject(&:empty?).join(' ')
   end
 end
