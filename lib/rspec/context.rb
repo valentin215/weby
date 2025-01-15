9c3f7b55ff7b2739b925bd63ef8ff5b25dbee632
Valentin Birault <valentin@hivebrite.com>
2025-01-15 19:44:24 +0100

    chore: context added

--- lib/rspec/context.rb
+++ lib/rspec/context.rb
@@ -2,8 +2,32 @@ module RSpec
   class Context
     def initialize(description)
       @description = description
+      @lets = {}
+      @before_hooks = []
+      @after_hooks = []
+    end
+
+    def let(name, &block)
+      @lets[name] = block
+
+      define_singleton_method(name) do
+        @memoized ||= {}
+        @memoized[name] ||= instance_eval(&block)
+      end
+    end
+
+    def before(&block)
+      @before_hooks << block
+    end
+
+    def after(&block)
+      @after_hooks << block
+    end
+
+    def run
+      # test
+      pp('Before hooks', @before_hooks.each { |hook| hook.call }, 'After hooks', @after_hooks.each { |hook| hook.call })
+      pp 'Lets', @lets
     end
   end
-
-  def run; end
 end
